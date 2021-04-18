40cbb3abdb30a822eff5e77425f2047f39426961
abhinay keesara <keesaraabhinay@gmail.com>
2021-04-17 16:25:50 -1000

    formatted code with black

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    book = models.CharField(max_length=100)
+    description = models.TextField()
+    read_by = models.DateField()
