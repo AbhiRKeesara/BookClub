f96577af11d3bba338d5d0c8f739da203ac94745
Abhinay Reddy Keesara <AbhiRKeesara@users.noreply.github.com>
2021-04-17 16:48:58 -1000

    created a book model and performed migrations to db (#3)

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# Each model represents an entity inside your application - e.g. a book
+# Each instance of your model represents a single row inside the database table.
+# https://docs.djangoproject.com/en/3.1/ref/models/fields/#field-types
 
 
 class Book(models.Model):
